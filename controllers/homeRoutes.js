e743544b92f4f8ea7e91038408568f5a3af19072
BrettBurrington <136997716+BrettBurrington@users.noreply.github.com>
2023-08-11 18:44:15 -0700

    Update homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { User, Post, Comment } = require('../models'); // Adjust the path to match your directory structure
+const { User, Post, Comment } = require('../models');
 
-// Define your routes and logic here
 
 module.exports = router;
 
 router.get('/', async (req, res) => {
-  try {
-    // Fetch data from your models and pass it to your template
+  t
     const blogPosts = await BlogPost.findAll();
-    res.render('homepage', { blogPosts }); // Replace 'homepage' with your template name
+    res.render('homepage', { blogPosts })
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'An error occurred while fetching data.' });
